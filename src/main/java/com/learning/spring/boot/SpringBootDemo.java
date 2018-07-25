1b0c78fa3904dc7d93b1ca4e15a8280004bc72aa
surendharmr <surendharmuthumani@gmail.com>
2018-07-25 22:36:11 +0530

    Initial commit

---
+++ src/main/java/com/learning/spring/boot/SpringBootDemo.java
@@ -0,0 +1,14 @@
+package com.learning.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootDemo.class, args);
+	}
+
+}
